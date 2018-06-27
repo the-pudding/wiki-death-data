69b3670381d93ab1dd1cbe5e8172a12d36f98ebe
russellgoldenberg <russellgoldenberg@gmail.com>
2018-06-27 10:51:18 -0400

    fix #2 use async await

--- scripts/get-people-pageviews.js
+++ scripts/get-people-pageviews.js
@@ -4,48 +4,41 @@ const pageviews = require('pageviews');
 const d3 = require('d3');
 const outputDir = './output/people-pageviews';
 
-function query(person, cb) {
-  const id = person.link.replace('/wiki/', '');
-  pageviews
-    .getPerArticlePageviews({
-      project: 'en.wikipedia',
-      agent: 'user',
-      granularity: 'daily',
-      start: '20150701',
-      end: '20180626',
-      article: person.name
-    })
-    .then(result => {
-      const output = d3.csvFormat(result.items);
-      fs.writeFileSync(`${outputDir}/${id}.csv`, output);
-      cb();
-    })
-    .catch(err => {
-      console.error(err);
-      cb();
-    });
+function query(person) {
+  return new Promise((resolve, reject) => {
+    const id = person.link.replace('/wiki/', '');
+    pageviews
+      .getPerArticlePageviews({
+        project: 'en.wikipedia',
+        agent: 'user',
+        granularity: 'daily',
+        start: '20150701',
+        end: '20180626',
+        article: person.name
+      })
+      .then(result => {
+        const output = d3.csvFormat(result.items);
+        fs.writeFileSync(`${outputDir}/${id}.csv`, output);
+        resolve();
+      })
+      .catch(reject);
+  });
 }
 
-function init() {
+async function init() {
   mkdirp(outputDir);
 
   const data = d3.csvParse(
     fs.readFileSync('./output/all-deaths-2015-2018.csv', 'utf-8')
   );
 
-  let i = 0;
-  let end = data.length;
-
-  const next = () => {
-    console.log(`${i} of ${end}`);
-    query(data[i], () => {
-      i += 1;
-      if (i < end) next();
-      else console.log('done');
-    });
-  };
-
-  next();
+  for (const item of data) {
+    await query(item)
+      .then(() => {
+        console.log(item.date_of_death);
+      })
+      .catch(console.error);
+  }
 }
 
 init();
